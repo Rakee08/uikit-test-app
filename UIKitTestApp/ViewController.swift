28e87516fa5e6709131eac4e2fc0a7e73d7a576c
Rakesh Rajashekaran <rakesh.rajashekaran@prudential.com.sg>
2026-03-11 18:42:36 +0530

    Initial UIKit app setup

---
+++ UIKitTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTestApp
+//
+//  Created by Rakesh R on 11/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
